3c512371fbc46732d1d6b7c2a73103fcb2cd7271
lxyhan <hanlyu2005@gmail.com>
2024-09-09 14:25:53 -0400

    added Reduce class

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int counter = 0;
+        int i = 100;
+        while(i>0) {
+            counter++;
+            if (i%2 ==0){
+                i = i/2;
+            } else {
+                i = i-1;
+            }
+        }
+        System.out.println(counter);
+    }
 }
